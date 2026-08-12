353a3be5c4444b4cab26779ca606e76b48a6f744
Adrian-Dz <dzieladrian@gmail.com>
2026-08-12 17:48:57 +0200

    wip

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -18,10 +18,14 @@ function init() {
       //Leaflet code
       var map = L.map('map').setView([51.505, -0.09], 13);
 
-      L.tileLayer('https://api.openstreetmap.org/api/0.6/map?bbox=14.106,48.959,24.258,54.997', {
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(map);
+
+      setTimeout(() => {
+        map.invalidateSize();
+    }, 100); 
     }
 }
 
