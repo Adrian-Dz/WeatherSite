fe43b972e526e1a516d8498cbd963f677886bf6d
Adrian-Dz <dzieladrian@gmail.com>
2026-08-12 17:20:07 +0200

    wip

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -11,7 +11,8 @@ function init() {
     if (weatherPanels.length > 0) {
         
     } else {
-
+      html = `<div id="map"></div>`
+      var map = L.map('map').setView([51.505, -0.09], 13);
     }
 }
 
