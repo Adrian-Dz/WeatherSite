427570f4fc3fdb1a5a1bf8b2927f98edf2396f15
Adrian-Dz <dzieladrian@gmail.com>
2026-08-12 17:26:37 +0200

    WIP leaflet

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -14,7 +14,14 @@ function init() {
       html = `<div id="map"></div>`
 
       mainPanel.innerHTML = html
+
+      //Leaflet code
       var map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map);
     }
 }
 
